653c98745476161aa1e52f044190344456687344
Timothy Higinbottom <timhigins@gmail.com>
2017-09-10 20:30:22 -0400

    Update build.sh
    [ci skip]

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 for arch in $archs; do
-  docker build -t tim15/qemu:$arch --build-arg ARCH=$arch .
+  docker build -t tim15/qemu:$arch --build-arg TARGET_ARCH=$arch .
   docker push tim15/qemu:$arch
 done;
